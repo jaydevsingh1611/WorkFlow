4398c87f5241ab53eef4aad9b41330f6a089cd13
jaydevsingh1611 <jaydevsingh1611@gmail.com>
2026-01-25 17:33:48 +0530

    First Commit

---
+++ server/config/instrument.js
@@ -0,0 +1,10 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+import * as Sentry from "@sentry/node";
+
+Sentry.init({
+  dsn: "https://afa907d7e624630dd3c50505f6bfb3fe@o4510766658682880.ingest.us.sentry.io/4510766670151680",
+  integrations: [Sentry.mongooseIntegration()],
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
---
+++ server/server.js
@@ -0,0 +1,32 @@
+import './config/instrument.js'
+import express from "express";
+import cors from 'cors';
+import 'dotenv/config'
+import connectDB from "./config/db.js";
+import * as Sentry from "@sentry/node";
+import { clearWebhooks } from './controllers/webhooks.js';
+
+// Initialize Express
+const app = express()
+
+// Connect to database
+await connectDB()
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+//Routes
+app.get('/' , (req,res) => res.send("API Working"))
+app.get("/debug-sentry", function mainHandler(req,res) {
+throw new Error("My first Sentry error!");
+});
+app.post('/webhooks',clearWebhooks)
+
+//Port
+const PORT = process.env.PORT || 5001
+Sentry.setupExpressErrorHandler(app);
+
+app.listen(PORT,() =>{
+console.log(`Server is running on ${PORT}`)
+})
